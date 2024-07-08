aaa36c3e5c4f9db93dc081ba0c7bb9779c037932
Aditya Narayan Jaiswal <nlaxmi886@gmail.com>
2024-07-08 22:57:06 +0530

    Added players class with methods and attributes, and Implemented dice roll service

---
+++ src/player.js
@@ -0,0 +1,29 @@
+class Player {
+    constructor(name, health, strength, attack) {
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+    }
+
+    attackRoll() {
+        return this.attack * rollDice();
+    }
+
+    defendRoll() {
+        return this.strength * rollDice();
+    }
+
+    takeDamage(damage) {
+        this.health -= damage;
+        if (this.health < 0) {
+            this.health = 0;
+        }
+    }
+
+    isAlive() {
+        return this.health > 0;
+    }
+}
+
+module.exports = Player;
